7a14c65e1ef241d55e51d71133ea8ca8b599b8b7
weishuwen <weishuwen@dkhs.com>
2016-09-02 16:11:23 +0800

    小修改

--- app/component/DrawerMenuComp.js
+++ app/component/DrawerMenuComp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-    View, Text, Image, StyleSheet
+    View, Text, Image, StyleSheet, TouchableNativeFeedback
 } from 'react-native';
 import { SIDE_LEFT_TABS } from '../constants/ActionTypes';
 import { APP_MAIN_COLOR } from '../constants/Styles';
@@ -10,10 +10,25 @@ class DrawerMenuComp extends React.Component {
         return (
             <View>
                 <Text>微信精选</Text>
+                <View>
+                    {this.renderMenuItem(SIDE_LEFT_TABS.HOME, '首页')}
+                    {this.renderMenuItem(SIDE_LEFT_TABS.CATEGORY, '标签')}
+                </View>
             </View>
         )
     }
     
+    renderMenuItem(tab, tabName) {
+        return (
+            <TouchableNativeFeedback
+                onPress={this.props.onTabSelect(tab)}
+                background={TouchableNativeFeedback.SelectableBackground()}>
+                <View>
+                    <Text style={{marginLeft: 30}}>{tabName}</Text>
+                </View>
+            </TouchableNativeFeedback>
+        )
+    }
 }
 
 export default DrawerMenuComp;
--- app/constants/ActionTypes.js
+++ app/constants/ActionTypes.js
@@ -1,8 +1,3 @@
-export const FETCH_ARTICLE_LIST = 'FETCH_ARTICLE_LIST';
-export const RECEIVE_ARTICLE_LIST = 'RECEIVE_ARTICLE_LIST';
-export const FETCH_TYPE_LIST = 'FETCH_TYPE_LIST';
-export const RECEIVE_TYPE_LIST = 'RECEIVE_TYPE_LIST';
-
 export const FETCH_ARTICLES_DATA_STATUS = {
     START: 'fetch_articles_data_start',
     SUCCESS: 'fetch_articles_data_success',
