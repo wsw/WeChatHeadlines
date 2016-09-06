e94a7c6e0bf0632613216fb6be131bd61f77e873
weishuwen <870224929@qq.com>
2016-09-06 22:03:49 +0800

    x

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -2,9 +2,32 @@ import React from 'react';
 import {
     View, Text
 } from 'react-native';
+import { connect } from 'react-redux';
+import fetchArticleList from '../actions/articles';
+// import CommonLoadView from '../component/common/LoadView';
 
-export default ()=>{
-    return (<View>
-            <Text>HEELO WORLD</Text>
-        </View>)
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.props.dispatch(fetchArticleList(1, 0, 1));
+    }
+
+    render() {
+        console.log(this.props.articles)
+        if (this.props.articles.isRefreshing) {
+            return <View><Text>Loading</Text></View>
+        }
+        return (<View>
+                {this.props.articles && this.props.articles.data.map(() => {
+                    return <Text>HEELO WORLD</Text>;
+                })}
+            </View>);
+    }
 }
+
+export default connect((store) => {
+    return {
+        articles: store.articles
+    }
+})(HomePage);;
